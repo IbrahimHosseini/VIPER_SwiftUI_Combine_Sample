0c09bf9d3d38a974c836d9c624e5b41f82674e94
Ibrahim Hosseini <sehosseini@me.com>
2024-03-27 12:56:11 +0330

    Initial Commit

---
+++ VIPER_SwiftUI_Combine_Sample/VIPER_SwiftUI_Combine_SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  VIPER_SwiftUI_Combine_SampleApp.swift
+//  VIPER_SwiftUI_Combine_Sample
+//
+//  Created by Ibrahim on 3/27/24.
+//
+
+import SwiftUI
+
+@main
+struct VIPER_SwiftUI_Combine_SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
